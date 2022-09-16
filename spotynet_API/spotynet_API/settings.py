8b357531ec052a2a9c36eb4510d00d612ada5966
Ricardo Mazuera <ricardo.mazuera@gmail.com>
2022-09-16 09:23:16 -0500

    modified setting

--- spotynet_API/spotynet_API/settings.py
+++ spotynet_API/spotynet_API/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = tuple(env.list('ALLOWED_HOSTS',default=[]))
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 CORS_ORIGIN_ALLOW_ALL = True
 
